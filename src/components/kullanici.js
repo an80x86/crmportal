5164d91cc8ef9acb0ca2fd06c5c62b450337566d
Mustafa Bayer <an80x86@gmail.com>
2018-04-25 12:02:41 +0300

    data tables eklendi

--- src/components/kullanici.js
+++ src/components/kullanici.js
@@ -1,9 +1,60 @@
-import React from 'react'
- 
-const Kullanici = () => (
-  <div>
-    <h1>Kullanici!</h1>
-  </div>
-)
+import React, {Component} from 'react'
+import ReactTable from 'react-table'
+import 'react-table/react-table.css'
+
+import { render } from "react-dom";
+import { makeData, Tips } from "./Utils";
+
+class Kullanici extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: makeData()
+    };
+  }
+  render() {
+    const { data } = this.state;
+    return (
+      <div>
+        <ReactTable
+          data={data}
+          columns={[
+            {
+              //Header: "Name",
+              columns: [
+                {
+                  Header: "First Name",
+                  accessor: "firstName"
+                },
+                {
+                  Header: "Last Name",
+                  id: "lastName",
+                  accessor: d => d.lastName
+                },
+                {
+                  Header: "Age",
+                  accessor: "age"
+                },
+                {
+                  Header: "Status",
+                  accessor: "status"
+                },
+                {
+                  Header: "Visits",
+                  accessor: "visits"
+                }
+              ]
+            },
+            
+          ]}
+          defaultPageSize={5}
+          className="-striped -highlight"
+        />
+        <br />
+        <Tips />
+      </div>
+    );
+  }
+}
 
 export default Kullanici
